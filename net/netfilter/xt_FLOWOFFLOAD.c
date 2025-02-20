4631c06e9257f57a2f992a19f9c09a5b1f13f71b
Misaka Nnnnq <149572432+Misaka-Nnnnq@users.noreply.github.com>
2025-02-21 03:15:36 +0800

    fix call to nf_flow_pppoe_proto from xt_FLOWOFFLOAD

--- net/netfilter/xt_FLOWOFFLOAD.c
+++ net/netfilter/xt_FLOWOFFLOAD.c
@@ -62,7 +62,8 @@ xt_flowoffload_net_hook(void *priv, struct sk_buff *skb,
 		proto = veth->h_vlan_encapsulated_proto;
 		break;
 	case htons(ETH_P_PPP_SES):
-		if (!nf_flow_pppoe_proto(skb, &proto))
+		proto = nf_flow_pppoe_proto(skb);
+		if (!proto)
 			return NF_ACCEPT;
 		break;
 	default:
